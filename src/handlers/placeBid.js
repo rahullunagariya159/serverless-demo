e7a09d19847f8693a76d1a5d9c789868d4155ebf
Rahul Lunagariya <rahul.lunagariya132@gmail.com>
2022-11-10 01:25:11 +0530

    Created a place bid api endpoint

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,35 @@
+import AWS from "aws-sdk";
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  const params = {
+    TableName: "AuctionsTable",
+    Key: { id },
+    UpdateExpression: "set highestBid.amount = :amount",
+    ExpressionAttributeValues: {
+      ":amount": amount,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200, //resource created status code
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
